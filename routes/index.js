1bcb337e0f48169a9756d9846877dc30a2c35f5f
ChoiJY <wnsdud0213@ajou.ac.kr>
2018-01-11 10:10:08 +0900

    Home Keyboard API update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// home keyboard implementation
+router.get('/keyboard', function (req, res) {
+  res.json({
+      "keyboard":{
+        "type":"buttons",
+        "buttons": ["ex1", "ex2", "ex3"]
+      }
+  })
+})
+
 module.exports = router;
