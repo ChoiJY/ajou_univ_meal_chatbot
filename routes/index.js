9dba6d4c4b31049bfb04db5025e8419991446689
ChoiJY <wnsdud0213@ajou.ac.kr>
2018-01-17 10:28:32 +0900

    gitignore add

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: kakao});
 });
 
 // home keyboard implementation
 router.get('/keyboard', function (req, res) {
-  res.json({
-      "type":"buttons",
-      "buttons": ["ex1", "ex2", "ex3"]
-  })
+
+    // keyboard layout form
+    const keyboard = {
+        "type": "buttons",
+        "buttons": ["ex1", "ex2", "ex3"]
+    };
+
+    res.json(keyboard);
 });
+
+router.post('/message', function (req, res) {
+    var userID = req.body.user_key;
+    var type = req.body.type;
+    var content = req.body.content;
+
+    res.json();
+})
 
 module.exports = router;
